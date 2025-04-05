65aa3df08c3dfd629ce773fa5ad49c29ab7e7391
Jhonny Rodriguez Martinez <true.jhonnyr@gmail.com>
2025-04-05 07:21:50 -0400

    Connect JS for logic, add new ToTop btn add new clients and fix some issues

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ document.addEventListener("DOMContentLoaded", () => {
     );
     track.style.width = totalWidth + "px";
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollToTopBtn = document.getElementById("scrollToTop");
+
+    // Detectar el evento de desplazamiento (scroll)
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 0) {
+            // Si el desplazamiento vertical es mayor a 0
+            scrollToTopBtn.style.display = "block"; // Muestra el botón
+        } else {
+            scrollToTopBtn.style.display = "none"; // Oculta el botón
+        }
+    });
+
+    // Función para desplazarse al tope de la página
+    scrollToTopBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth", // Desplazamiento suave
+        });
+    });
+});
